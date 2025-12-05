390c8d10a04fcf40f9e7366dee1a9cc0c899d746
Nicolás <a@MacBook-Pro-de-Nicolas.local>
2025-12-05 18:07:06 -0500

    modficacion del proceso del servidor

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
